1cd3778dbcb6a3be8e8bec9a75bfea221feeef74
Roman Makeev <makeevrserg@gmail.com>
2024-06-15 15:42:06 +0300

    add items adder title item

--- modules/api/src/bukkitMain/kotlin/ru/astrainteractive/astrashop/api/model/SpigotTitleItemStack.kt
+++ modules/api/src/bukkitMain/kotlin/ru/astrainteractive/astrashop/api/model/SpigotTitleItemStack.kt
@@ -2,9 +2,17 @@ package ru.astrainteractive.astrashop.api.model
 
 import org.bukkit.Material
 
-data class SpigotTitleItemStack(
-    val material: Material,
-    val customModelData: Int = 0,
-    val name: String,
-    val lore: List<String> = emptyList()
-) : TitleItemStack
+sealed interface SpigotTitleItemStack : TitleItemStack {
+    data class Default(
+        val material: Material,
+        val customModelData: Int = 0,
+        val name: String,
+        val lore: List<String> = emptyList()
+    ) : SpigotTitleItemStack
+
+    data class ItemsAdder(
+        val namespaceId: String,
+        val name: String,
+        val lore: List<String> = emptyList()
+    ) : SpigotTitleItemStack
+}
