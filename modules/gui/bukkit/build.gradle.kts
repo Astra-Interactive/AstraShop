26aa5c45f96270c5b9550171f7a176dea42cdae0
Roman Makeev <makeevrserg@gmail.com>
2024-12-13 11:50:42 +0300

    fix dependencies

--- modules/gui/bukkit/build.gradle.kts
+++ modules/gui/bukkit/build.gradle.kts
@@ -8,7 +8,6 @@ dependencies {
     implementation(libs.bundles.kotlin)
     // AstraLibs
     implementation(libs.minecraft.astralibs.core)
-    implementation(libs.minecraft.astralibs.orm)
     implementation(libs.klibs.mikro)
     implementation(libs.minecraft.astralibs.menu.bukkit)
     implementation(libs.minecraft.astralibs.core.bukkit)
